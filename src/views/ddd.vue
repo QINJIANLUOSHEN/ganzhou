342f94352321a5e2bf6fde5f3e8686e3005552ed
朱庆春 <zhuqch@internal.ths.com.cn>
2021-01-13 16:48:10 +0800

    feat：提交初版

---
+++ src/views/ddd.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: '组件名',
+  setup() {
+    return {
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
