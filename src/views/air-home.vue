2a45e59c0c73304b95eecff21ed9150123677ac6
zhengxd <zhengxd@internal.ths.com.cn>
2023-04-08 10:26:30 +0800

    feat: 新增空气质量指数背景图

--- src/views/air-home.vue
+++ src/views/air-home.vue
@@ -1,12 +1,20 @@
 <!--
  * @Author: yushuang
  * @Date: 2023-04-07 16:54:55
- * @LastEditors: yushuang
- * @LastEditTime: 2023-04-07 23:34:10
+ * @LastEditors: zhengxd zhengxd@internal.ths.com.cn
+ * @LastEditTime: 2023-04-08 10:25:21
  * @Description:
 -->
 <template>
-  <div>空气质量指数</div>
+  <!-- 空气质量指数盒子 -->
+  <div class="air-quality-box">
+    <!-- 动画云 -->
+    <div class="animation-cloud"></div>
+    <!-- 标题 -->
+    <div class="air-title">
+      赣州市空气质量指数/AQI
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -21,4 +29,21 @@ export default defineComponent({
 </script>
 
 <style>
+/* 整体背景 */
+.air-quality-box {
+  position: relative;
+  width: 1014px;
+  height: 540px;
+  background: url(@/assets/images/air-quality-bg.png) no-repeat center;
+}
+
+/* 动画云 */
+.animation-cloud {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 1014px;
+  height: 540px;
+  background: url(@/assets/images/air-quality-cloud.png) no-repeat center;
+}
 </style>
